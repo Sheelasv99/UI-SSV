81f8351824420fe2fa503eb0d4ef0694662b66e6
sheelasupriyav <vssupriya99@gmail.com>
2024-06-26 09:07:06 +0530

    latest files updated

--- index.js
+++ index.js
@@ -28,5 +28,5 @@ app.use(function(req,res,next){
 app.use('/user',userRoutes);
 app.use('/booking',bookingRoutes);
 app.use('/feedback',feedbackRoutes);
-const PORT =process.env.PORT || 3000;
+const PORT =process.env.PORT || 5000;
 app.listen(PORT,() => console.log('Server started on port ' +PORT));
