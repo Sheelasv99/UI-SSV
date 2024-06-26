7dbd62831cbaebc29acab74a9b34eafac0bc7b96
sheelasupriyav <vssupriya99@gmail.com>
2024-06-26 19:13:57 +0530

    updated files

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 
 import './App.css';
-import Booking from "../components/Booking.js";
+import Booking from  './components/Booking.js';
 
-function App() {
+function App (){
   return (
     <div className="App">
       <h1>welcome To SS Resorts Booking!!</h1>
       <Booking/>
     </div>
   );
-}
+};
 
 export default App;
--- client/src/components/Booking.js
+++ client/src/components/Booking.js
@@ -1,30 +1,38 @@
 const user ={
-     id:2345,
-     username : "sheela"
+    id:2345,
+    username : "sheela"
 }
 
-const booking=[
-    {
-        id:1234,
-        roomtype:"TQNN"
-    },
-    {
-        id:4567,
-        roomtype:"KNGN"
-    },
-    {
-        id:6543,
-        roomtype:"SUITE ROOM TQNN"
-    },
+const booking = [
+   {
+       id:1234,
+       roomtype:"TQNN"
+   },
+   {
+       id:4567,
+       roomtype:"KNGN"
+   },
+   {
+       id:6543,
+       roomtype:"SUITE ROOM TQNN"
+   },
 ]
 const Booking=() =>{
-    return {
-       <div>
-          <h2>{user.username}'s favourite room </h2>
+   return (
+     <div>
+         <h2>{user.username}'s favourite room </h2>
+        <ul>
+           {booking.map((booking) => (
+            <li key={booking.id}>{booking.roomtype}</li>
+           ))
+           
+           }
+        </ul>
        </div>
+        
+    
+   );
+};
 
-         
-     
-    };
-}
+
 export default Booking;
